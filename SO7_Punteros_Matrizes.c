35e701de64366020da49d5971d3285640d516df1
Nicolas11Leon <leonparranicolas@gmail.com>
2024-08-18 14:12:48 -0500

    SO7_Punteros_Matrizes.c

---
+++ SO7_Punteros_Matrizes.c
@@ -0,0 +1,46 @@
+/*********************************************************************
+Autor:   Nicolas leon. 
+Materia: Sistemas operativos.
+Fecha:   25-07-2024.
+Tema:    Punteros y Matrizes
+**********************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define size 10
+
+int main(int argc, char ** argv){
+
+  int rows, cols, i, j;
+  int **matrix;
+  
+  rows = (int)atof(argv[1]);
+  cols = (int)atof(argv[2]);
+  
+  matrix = (int **)malloc(rows*sizeof(int *));
+  for (i=0; i<rows; i++){
+      matrix[i] =(int *)malloc(cols * sizeof(int));
+    
+  }
+  for (i=0; i<rows; i++){
+  for(j=0; j<cols; j++){
+    matrix[i][j]= i * j;
+   
+  }}
+
+for (i=0; i< rows; i++){
+   for(j=0; j<cols; j++){
+     printf("%d", matrix[i][j]);
+}
+  printf("\n");
+  
+
+}
+
+
+ for(i=0; i<rows; i++){
+   free(matrix[i]);
+ }
+  free(matrix);
+  return 0;
+}
