e795ebd87bb8f7c252ed9d563ce378c66f8570ba
Nicolas11Leon <leonparranicolas@gmail.com>
2024-08-18 14:14:22 -0500

    example0.c

---
+++ example0.c
@@ -0,0 +1,25 @@
+/*********************************************************************
+Autor:   Nicolas leon. 
+Materia: sistemas operativos.
+Fecha:   18-07-2024.
+Tema:    Introduccion allenguaje C y los entornos.
+         -Entorno en la web(cloud/Nube).
+         -Comandos principales de Linux.
+         -Programa 1: Bloques basicos de la programacion.
+**********************************************************************/
+
+#include <stdio.h>
+
+
+int main(int arcg, char *argv[]){
+  
+  printf("\n Hola me llamo Nicolas Leon \n **************** \n Estudio Ingenieria de sistemas y mecatronica, este semestre solo meti media matricula de sistemas \n ~~~~~~~~~~~~~~~ \n estoy viendo: \n Proyecto T.I. \n Sistemas operativos. \n Analisis y DiseÃ±o de software. \n Teoria de la computacion, ");
+  
+  
+  
+  
+  return 0;
+}
+
+
+
