5c41094b0edd1634c728ef127424e44c52eedfb2
Nicolas11Leon <leonparranicolas@gmail.com>
2024-08-18 14:15:13 -0500

    funciones.h

---
+++ funciones.h
@@ -0,0 +1,21 @@
+/*********************************************************************
+Autor:   Nicolas leon. 
+Materia: Sistemas operativos.
+Fecha:   25-07-2024.
+Tema:    Entrada y Salida en c
+**********************************************************************/
+
+#include <stdio.h>
+float pi=3.141516;
+
+  void sumarEneteros();
+  
+  void  Calcularareayvolumendcirculos(); 
+  
+  void Calcularvoumendcilindr();
+
+  void calcuartangente();
+
+  
+  
+  
